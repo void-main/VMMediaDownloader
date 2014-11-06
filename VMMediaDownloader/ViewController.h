648aff47b9682a7d8e5d538c503fe75ac3530eb7
Void Main <voidmain1313113@gmail.com>
2014-11-06 09:57:51 +0800

    Initial Commit

---
+++ VMMediaDownloader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VMMediaDownloader
+//
+//  Created by Sun Peng on 11/6/14.
+//  Copyright (c) 2014 Peng Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VMMediaDownloader/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VMMediaDownloader
+//
+//  Created by Sun Peng on 11/6/14.
+//  Copyright (c) 2014 Peng Sun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
